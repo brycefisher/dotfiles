38b2899eca1116707eaa8938e6d718b51f591a07
Bryce <hckr1292@gmail.com>
2016-07-09 16:57:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Dotfiles
+--------
+
+Objectives:
+ - Requires only bash
+ - Works on OSX (probably not anywhere else, like Ubuntu)
+
+Bootstrapping:
+
+```
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+if [ ! -f "$(pwd)/git-completion.bash" ]; then
+	echo 'Downloading git completions for bash'
+	curl -sSO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+fi
+
+echo 'Ensuring the bashrc file in this directory is always sourced in .bashrc'
+SOURCE_BASHRC="source $(pwd)/bashrc"
+grep -q "$SOURCE_BASHRC" "$HOME/.profile" || echo "$SOURCE_BASHRC" >> "$HOME/.profile"
