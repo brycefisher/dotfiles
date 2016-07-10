e58411656792052b50cde1a5fb831f3883f5fcdc
Bryce Fisher-Fleig <hckr1292@gmail.com>
2016-07-09 17:09:26 -0700

    Added .gitconfig

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ fi
 echo 'Ensuring the bashrc file in this directory is always sourced in .bashrc'
 SOURCE_BASHRC="source $(pwd)/bashrc"
 grep -q "$SOURCE_BASHRC" "$HOME/.profile" || echo "$SOURCE_BASHRC" >> "$HOME/.profile"
+
+echo 'Symlink git configuration'
+rm -f "$HOME/.gitconfig"
+ln -s "$(pwd)/gitconfig" "$HOME/.gitconfig"
